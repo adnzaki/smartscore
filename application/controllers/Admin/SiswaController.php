7665f56bab5973cd53c427cdbbf32f37693f6b6e
Adnan Zaki <adnan_zaki@hotmail.co.id>
2017-05-23 11:07:51 +0700

    Mengembalikan form_validation ke controller.

--- application/controllers/Admin/SiswaController.php
+++ application/controllers/Admin/SiswaController.php
@@ -38,6 +38,92 @@ class SiswaController extends CI_Controller
 
     public function setSiswa($event, $key = '')
     {
+        if($event === 'insert')
+        {
+            $is_unique = [
+                '|is_unique[siswa.nis]',
+                '|is_unique[siswa.nisn]',
+            ];
+        }
+        else
+        {
+            $is_unique = ['', ''];
+        }
+
+        $rules = [
+            [
+                'field' => 'nama_siswa',
+                'label' => 'nama siswa',
+                'rules' => 'required|max_length[100]'
+            ],
+            [
+                'field' => 'nis',
+                'label' => 'Nomor Induk Sekolah',
+                'rules' => 'required'.$is_unique[0].'|exact_length[9]|numeric'
+            ],
+            [
+                'field' => 'nisn',
+                'label' => 'NISN',
+                'rules' => 'required'.$is_unique[1].'|exact_length[10]|numeric'
+            ],
+            [
+                'field' => 'tempat_lahir_siswa',
+                'label' => 'tempat lahir',
+                'rules' => 'required|max_length[100]'
+            ],
+            [
+                'field' => 'tgl_lahir_siswa',
+                'label' => 'tanggal lahir',
+                'rules' => 'required|exact_length[10]'
+            ],
+            [
+                'field' => 'pend_sblm',
+                'label' => 'pendidikan sebelumnya',
+                'rules' => 'max_length[100]'
+            ],
+            [
+                'field' => 'alamat_siswa',
+                'label' => 'alamat siswa',
+                'rules' => 'required|max_length[250]'
+            ],
+            [
+                'field' => 'nama_ayah',
+                'label' => 'nama ayah',
+                'rules' => 'required|max_length[100]'
+            ],
+            [
+                'field' => 'nama_ibu',
+                'label' => 'nama ibu',
+                'rules' => 'required|max_length[100]'
+            ],
+            [
+                'field' => 'alamat_ortu',
+                'label' => 'alamat orang tua',
+                'rules' => 'required|max_length[255]'
+            ],
+            [
+                'field' => 'telp_ortu',
+                'label' => 'nomor telepon orang tua',
+                'rules' => 'required|min_length[11]|max_length[15]|numeric'
+            ],
+            [
+                'field' => 'nama_wali',
+                'label' => 'nama wali',
+                'rules' => 'max_length[50]'
+            ],
+            [
+                'field' => 'alamat_wali',
+                'label' => 'alamat wali',
+                'rules' => 'max_length[255]'
+            ],
+            [
+                'field' => 'telp_wali',
+                'label' => 'nomor telepon wali',
+                'rules' => 'min_length[11]|max_length[15]|numeric'
+            ],
+        ];
+
+        $this->form_validation->set_rules($rules);
         $this->form_validation->set_message('required', 'Kolom {field} wajib diisi');
         $this->form_validation->set_message('max_length', 'Panjang kolom %s tidak boleh lebih dari %s karakter');
         $this->form_validation->set_message('min_length', 'Panjang kolom %s minimal %s karakter');
