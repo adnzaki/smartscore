fc7c2c061c600fd7031b5258d060a5dbf5760f77
Adnan Zaki <adnan_zaki@hotmail.co.id>
2017-06-22 14:43:56 +0700

    Loader kini tampil full screen

--- application/views/home.php
+++ application/views/home.php
@@ -9,13 +9,13 @@
 </head>
 
 <body>
+	<?php $this->view('content/loader') ?>
 	<div class="app" id="app">
 		<?php $this->view('elements/sidebar') ?>
 		<div id="content" class="app-content box-shadow-z0" role="main">
 			<?php $this->view('elements/app-header') ?>
 			<?php $this->view('elements/app-footer') ?>
 			<div ui-view class="app-body" id="view">
-				<?php $this->view('content/loader') ?>
 				<div id="beranda">
 					<h2 v-if="showDashboard">Halaman dashboard</h2>
 				</div>
