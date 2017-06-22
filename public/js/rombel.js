bf075ab3a2bd12b37ea72cedc774bd2ae62ed3f6
Adnan Zaki <adnan_zaki@hotmail.co.id>
2017-06-22 14:31:42 +0700

    Melengkapi deskripsi dan lisensi aplikasi

--- public/js/rombel.js
+++ public/js/rombel.js
@@ -1,4 +1,13 @@
-
+/**
+ * Smartscore
+ * Aplikasi Pengolahan Nilai Siswa berbasis Kurikulum 2013 untuk tingkat Sekolah Dasar (SD)
+ *
+ * @copyright   Copyright (c) 2017, Adnan Zaki
+ * @license     Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International Public License | https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
+ * @author      Adnan Zaki
+ * @link        http://wolestech.com
+ * @version     1.0.0
+ */
 
 var rombel = new Vue({
     el: '#dataRombel',
