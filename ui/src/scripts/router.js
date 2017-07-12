0e129b3c99d920f29f4b9c860c7afa276bddc9e6
Adnan Zaki <adnan_zaki@hotmail.co.id>
2017-07-12 18:59:53 +0700

    Menambah link ke halaman rombel dan menjadikan dashboard sebagai halamana utama

--- ui/src/scripts/router.js
+++ ui/src/scripts/router.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import dashboard from '../template/pages/beranda/dashboard.vue'
 import Siswa from '../template/pages/siswa/Siswa.vue'
+import Rombel from '../template/pages/rombel/Rombel.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-    { path: '/dashboard', component: dashboard },
-    { path: '/siswa/', component: Siswa }
+    { path: '/', component: dashboard },
+    { path: '/siswa', component: Siswa },
+    { path: '/rombel', component: Rombel }
 ]
 
 const router = new VueRouter({
