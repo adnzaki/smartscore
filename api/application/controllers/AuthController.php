e07595a88477a3200c2877a4dfef78a79da5ee35
Adnan Zaki <adnan_zaki@hotmail.co.id>
2017-09-27 16:24:47 +0700

    Menambah validasi form login di sisi server

--- api/application/controllers/AuthController.php
+++ api/application/controllers/AuthController.php
@@ -14,26 +14,56 @@ class AuthController extends SSController
     {
         $user = $this->input->post('username', true);
         $pass = $this->input->post('password', true);
-        if(AuthModel::isValidUser($user, $pass)) 
-        {            
-            $token = [
-                'username' => $user,
-                'tahun_ajaran' => $this->input->post('tahun_ajaran', true)
-            ];
-            $encoded = JWT::encode($token, 'user_key');
+        $rules = [
+            [
+                'field' => 'username',
+                'label' => 'Username',
+                'rules' => 'required'
+            ],
+            [
+                'field' => 'password',
+                'label' => 'Password',
+                'rules' => 'required'
+            ],
+            [
+                'field' => 'tahun_ajaran',
+                'label' => 'Tahun Ajaran',
+                'rules' => 'required'
+            ]
+        ];
+        $this->form_validation->set_rules($rules);
+        $this->form_validation->set_message('required', '{field} wajib diisi');
+        $this->form_validation->set_error_delimiters('', '');
+        if($this->form_validation->run() == FALSE)
+        {
             $res = [
-                'code' => 1,
-                'msg' => 'Login berhasil, mengalihkan halaman...',
-                'cookie' => $encoded
+                'code'  => 0,
+                'msg'   => 'Invalid login'
             ];
         }
         else 
         {
-            $res = [
-                'code' => 0,
-                'msg' => 'Username atau password yang anda masukkan salah'
-            ];
-        }
+            if(AuthModel::isValidUser($user, $pass)) 
+            {            
+                $token = [
+                    'username'      => $user,
+                    'tahun_ajaran'  => $this->input->post('tahun_ajaran', true)
+                ];
+                $encoded = JWT::encode($token, 'user_key');
+                $res = [
+                    'code'      => 1,
+                    'msg'       => 'Login berhasil, mengalihkan halaman...',
+                    'cookie'    => $encoded
+                ];
+            }
+            else 
+            {
+                $res = [
+                    'code'  => 0,
+                    'msg'   => 'Username atau password yang anda masukkan salah'
+                ];
+            }
+        }        
 
         echo json_encode($res);
     }
