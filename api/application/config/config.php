798964ae2c3e292c92fbf199a978e5d796625a29
Adnan Zaki <adnan_zaki@hotmail.co.id>
2018-01-11 15:00:28 +0700

    BaseURL CodeIgniter kini telah full dinamis

--- api/application/config/config.php
+++ api/application/config/config.php
@@ -23,9 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$root_script        = explode("/", $_SERVER['PHP_SELF']);
-$doc_root           = $root_script[1]."/".$root_script[2];
-$config['base_url'] = (is_https() ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'].'/'.$doc_root.'/';
+$root = (is_https() ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $root;
 
 /*
 |--------------------------------------------------------------------------
