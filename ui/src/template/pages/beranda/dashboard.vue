681f799f61cbbbdad57a8f7eb30217d39be5f336
Adnan Zaki <adnan_zaki@hotmail.co.id>
2018-03-26 10:38:52 +0700

    Menambahkan template untuk dashboard

--- ui/src/template/pages/beranda/dashboard.vue
+++ ui/src/template/pages/beranda/dashboard.vue
@@ -1,6 +1,214 @@
 <template lang="html">
-  <div class="">
-      <h1>{{ pesan }}</h1>
+  <div>
+      <div class="padding">
+		<div class="row">
+			<div class="col-xs-12 col-sm-4">
+				<div class="box p-a">
+					<div class="pull-left m-r">
+						<span class="w-48 rounded  accent">
+							<i class="material-icons">&#xe151;</i>
+						</span>
+					</div>
+					<div class="clear">
+						<h4 class="m-a-0 text-lg _300"><a href>20 <span class="text-sm">Siswa</span></a></h4>
+						<small class="text-muted">6 new arrivals.</small>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-6 col-sm-4">
+				<div class="box p-a">
+					<div class="pull-left m-r">
+						<span class="w-48 rounded primary">
+							<i class="material-icons">&#xe54f;</i>
+						</span>
+					</div>
+					<div class="clear">
+						<h4 class="m-a-0 text-lg _300"><a href>5 <span class="text-sm">Nominasi</span></a></h4>
+						<small class="text-muted">38 open.</small>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-6 col-sm-4">
+				<div class="box p-a">
+					<div class="pull-left m-r">
+						<span class="w-48 rounded warn">
+							<i class="material-icons">&#xe8d3;</i>
+						</span>
+					</div>
+					<div class="clear">
+						<h4 class="m-a-0 text-lg _300"><a href>3 <span class="text-sm">Pengguna</span></a></h4>
+						<small class="text-muted">632 vips.</small>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- batasssss -->
+		<div class="row">
+			<div class="col-sm-6 col-md-4">
+				<div class="box">
+					<div class="box-header">
+					<h3>Members</h3>
+					</div>
+					<ul class="list no-border p-b">
+					<li class="list-item">
+						<a herf class="list-left">
+							<span class="w-40 avatar">
+							<img src="../../../../src/assets/images/a4.jpg" alt="...">
+							<i class="on b-white bottom"></i>
+							</span>
+						</a>
+						<div class="list-body">
+						<div><a href>Chris Fox</a></div>
+						<small class="text-muted text-ellipsis">Designer, Blogger</small>
+						</div>
+					</li>
+					<li class="list-item">
+						<a herf class="list-left">
+						<span class="w-40 avatar">
+							<img src="../../../../src/assets/images/a5.jpg" alt="...">
+							<i class="on b-white bottom"></i>
+						</span>
+						</a>
+						<div class="list-body">
+						<div><a href>Mogen Polish</a></div>
+						<small class="text-muted text-ellipsis">Writter, Mag Editor</small>
+						</div>
+					</li>
+					<li class="list-item">
+						<a herf class="list-left">
+						<span class="w-40 avatar">
+							<img src="../../../../src/assets/images/a6.jpg" alt="...">
+							<i class="away b-white bottom"></i>
+						</span>
+						</a>
+						<div class="list-body">
+						<div><a href>Joge Lucky</a></div>
+						<small class="text-muted text-ellipsis">Art director, Movie Cut</small>
+						</div>
+					</li>
+					<li class="list-item">
+						<a herf class="list-left">
+						<span class="w-40 avatar">
+							<img src="../../../../src/assets/images/a7.jpg" alt="...">
+							<i class="busy b-white bottom"></i>
+						</span>
+						</a>
+						<div class="list-body">
+						<div><a href>Folisise Chosielie</a></div>
+						<small class="text-muted text-ellipsis">Musician, Player</small>
+						</div>
+					</li>
+					<li class="list-item">
+						<a herf class="list-left">
+							<span class="w-40 avatar success">
+							<span>P</span>
+							<i class="away b-white bottom"></i>
+							</span>
+						</a>
+						<div class="list-body">
+						<div><a href>Peter</a></div>
+						<small class="text-muted text-ellipsis">Musician, Player</small>
+						</div>
+					</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-sm-6 col-md-4">
+				<div class="box">
+					<div class="box-header">
+						<h3>Tasks</h3>
+						<small>20 finished, 5 remaining</small>
+					</div>
+					<div class="box-tool">
+						<ul class="nav">
+						<li class="nav-item inline dropdown">
+							<a class="nav-link text-muted p-x-xs" data-toggle="dropdown">
+							<i class="fa fa-ellipsis-v"></i>
+							</a>
+							<div class="dropdown-menu dropdown-menu-scale pull-right">
+							<a class="dropdown-item" href>New task</a>
+							<a class="dropdown-item" href>Make all finished</a>
+							<a class="dropdown-item" href>Make all unfinished</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item">Settings</a>
+							</div>
+						</li>
+						</ul>
+					</div>
+					<div class="box-body">
+						<div class="streamline b-l m-l">
+							<div class="sl-item b-success">
+							<div class="sl-icon">
+								<i class="fa fa-check"></i>
+							</div>
+							<div class="sl-content">
+								<div class="sl-date text-muted">8:30</div>
+								<div>Call to customer <a href class="text-info">Jacob</a> and discuss the detail about the AP project.</div>
+							</div>
+							</div>
+							<div class="sl-item b-info">
+							<div class="sl-content">
+								<div class="sl-date text-muted">Sat, 5 Mar</div>
+								<div>Prepare for presentation</div>
+							</div>
+							</div>
+							<div class="sl-item b-warning">
+							<div class="sl-content">
+								<div class="sl-date text-muted">Sun, 11 Feb</div>
+								<div><a href class="text-info">Jessi</a> assign you a task <a href class="text-info">Mockup Design</a>.</div>
+							</div>
+							</div>
+						</div>
+					</div>
+					<div class="box-footer">
+						<a href class="btn btn-sm warn text-u-c pull-right">Add one</a>
+						<a href class="btn btn-sm white text-u-c">More</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-12 col-md-4">
+				<div class="box">
+					<div class="box-header">
+						<span class="label success pull-right">5</span>
+						<h3>Activity</h3>
+						<small>10 members update their activies.</small>
+					</div>
+					<div class="box-body">
+						<div class="streamline b-l m-b m-l">
+						<div class="sl-item">
+							<div class="sl-left">
+							<img src="../../../../src/assets/images/a2.jpg" class="img-circle">
+							</div>
+							<div class="sl-content">
+							<a href class="text-info">Louis Elliott</a><span class="m-l-sm sl-date">5 min ago</span>
+							<div>assign you a task <a href class="text-info">Mockup Design</a>.</div>
+							</div>
+						</div>
+						<div class="sl-item">
+							<div class="sl-left">
+							<img src="../../../../src/assets/images/a5.jpg" class="img-circle">
+							</div>
+							<div class="sl-content">
+							<a href class="text-info">Terry Moore</a><span class="m-l-sm sl-date">10 min ago</span>
+							<div>Follow up to close deal</div>
+							</div>
+						</div>
+						<div class="sl-item">
+							<div class="sl-left">
+							<img src="../../../../src/assets/images/a8.jpg" class="img-circle">
+							</div>
+							<div class="sl-content">
+							<a href class="text-info">Walter Paler</a><span class="m-l-sm sl-date">1 hour ago</span>
+							<div>was added to Repo</div>
+							</div>
+						</div>
+						</div>
+						<a href class="btn btn-sm white text-u-c m-y-xs">Load More</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	  </div>
   </div>
 </template>
 <script>
