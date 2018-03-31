c3510021cbd353c88c68029022bd4b4e648c0da9
Adnan Zaki <adnan_zaki@hotmail.co.id>
2018-03-31 22:12:05 +0700

    Mengganti text-footer

--- ui/src/template/elements/app-footer.vue
+++ ui/src/template/elements/app-footer.vue
@@ -2,7 +2,7 @@
     <div class="app-footer">
       <div class="p-a text-xs">
         <div class="pull-right text-muted">
-          &copy; Copyright (2018) <strong>Smartscore App</strong> <span class="hidden-xs-down">- Created by Adnan Zaki</span>
+          &copy; Copyright (2018) <strong>Smartscore App</strong> <span class="hidden-xs-down">- Developed by Adnan Zaki</span>
           <a ui-scroll-to="content"><i class="fa fa-long-arrow-up p-x-sm"></i></a>
         </div>
       </div>
